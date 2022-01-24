a0833c906b24380063b201112b19791c0c645fd6
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-01-24 11:53:47 +0100

    Add test to only pay valid employees

--- src/test/java/com/example/bank/EmployeeManagerTest.java
+++ src/test/java/com/example/bank/EmployeeManagerTest.java
@@ -2,7 +2,10 @@ package com.example.bank;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
 class EmployeeManagerTest {
 
@@ -16,6 +19,24 @@ class EmployeeManagerTest {
         var expected = employeeRepositoryStub.findAll().size();
 
         assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void payEmployeesShouldOnlyPayValidEmployees() {
+        EmployeeRepository employeeRepository = mock(EmployeeRepository.class);
+        when(employeeRepository.findAll()).thenReturn(List.of(
+                new Employee("200", 25750.00),
+                new Employee("201", 26500.00)
+        ));
+        BankService bankService = mock(BankService.class);
+        doThrow(RuntimeException.class).when(bankService).pay("201", 26500.00);
+
+        EmployeeManager employeeManager = new EmployeeManager(employeeRepository, bankService);
+
+
+        var result = employeeManager.payEmployees();
+
+        assertThat(result).isEqualTo(1);
     }
 
 }
