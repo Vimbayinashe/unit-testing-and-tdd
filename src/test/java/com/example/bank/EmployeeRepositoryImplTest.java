2cb09589e02211c5695b3652c26973e186bb7cf1
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-01-24 13:30:17 +0100

    Add test that findAll initially returns empty list

--- src/test/java/com/example/bank/EmployeeRepositoryImplTest.java
+++ src/test/java/com/example/bank/EmployeeRepositoryImplTest.java
@@ -3,10 +3,16 @@ package com.example.bank;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeRepositoryImplTest {
 
-    
+    @Test
+    void findAllShouldInitiallyReturnAnEmptyList() {
+        EmployeeRepository employeeRepository = new EmployeeRepositoryImpl();
+
+        var result = employeeRepository.findAll();
+
+        assertThat(result).isEmpty();
+    }
 
 }
